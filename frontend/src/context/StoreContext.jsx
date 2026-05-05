abf794d39a3b3a1f4b0b9adb2940cdf632c0d3ee
deval1804 <devaldarji93795@gmail.com>
2026-05-05 15:03:56 +0530

    "Added Url In StoredContext"

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+ const url = "https://food-delivery-app-1pzb.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFood_List] = useState([]);
 
